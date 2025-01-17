08e735d01aab4e9d660dc922a5046fb090d701b9
Mathis ROUBILLE <mathis.roubille@etu.uca.fr>
2025-01-18 00:52:26 +0100

    Add qccess memory addresses

--- AccessMemory.py
+++ AccessMemory.py
@@ -1,4 +1,5 @@
 from MemoryAdresse import *
+from Constante import *
 
 # Function to get Pokémon levels in your party
 def get_pokemon_party_levels(pyboy):
@@ -48,7 +49,56 @@ def get_pos(pyboy):
     
     return x_pos, y_pos, map_n
 
-def set_pos(pyboy, x, y, map_n):
-    pyboy.memory[X_POS_ADDRESS] = x
-    pyboy.memory[Y_POS_ADDRESS] = y
-    pyboy.memory[MAP_N_ADDRESS] = map_n
+def get_enemy_pokemons(pyboy):
+    enemy_pokemons = []
+    total_enemy_pokemon = pyboy.memory[TOTAL_ENEMY_POKEMON]
+    for i in range(total_enemy_pokemon):
+        enemy_pokemon_id = pyboy.memory[ENEMY_POKEMONS[i]]
+        enemy_pokemons.append(enemy_pokemon_id)
+    return enemy_pokemons
+
+def get_enemy_total_pokemon(pyboy):
+    return pyboy.memory[TOTAL_ENEMY_POKEMON]
+
+def pokemon_id_to_name(pokemon_id):
+    return poke_id_to_name[pokemon_id]
+
+def get_hp(adr_low, adr_high, pyboy):
+    hp_low = pyboy.memory(adr_low)
+    hp_high = pyboy.memory(adr_high)
+    
+    hp = (hp_high << 8) | hp_low
+    return hp
+
+def get_enemy_hp(pyboy):
+    return get_hp(ENEMY_HP_ADDRESS_LOW, ENEMY_HP_ADDRESS_HIGH, pyboy)
+
+def get_enemy_max_hp(pyboy):
+    return get_hp(ENEMY_MAX_HP_ADDRESS_LOW, ENEMY_MAX_HP_ADDRESS_HIGH, pyboy)
+
+def get_enemy_level(pyboy):
+    return pyboy.memory(ENEMY_LVL)
+
+def get_pokemon_info(pyboy, pokemon_index):
+    base_address = PLAYER_POKEMONS[pokemon_index]
+    
+    current_hp = get_hp(base_address + 1, base_address + 2, pyboy)
+    max_hp = get_hp(base_address + 0x1A, base_address + 0x1B, pyboy)
+    level = pyboy.memory[base_address + 3]
+    status = pyboy.memory[base_address + 4]
+    moves_list = [pyboy.memory[base_address + 0x0C + i] for i in range(4)]
+    pp_moves_list = [pyboy.memory[base_address + 0x1D + i] for i in range(4)]
+    
+    moves_info = {f"move_{i+1}": moves[moves_list[i]] for i in range(4)}
+    pp_moves_info = {f"pp_move_{i+1}": pp_moves_list[i] for i in range(4)}
+    
+    pokemon_info = {
+        "current_hp": current_hp,
+        "max_hp": max_hp,
+        "level": level,
+        "status": status,
+        "moves": moves_info,
+        "pp_moves": pp_moves_info
+    }
+    
+    return pokemon_info
